8ddebd1d47a04fec509ef367d8ee3f703336bfd5
Gabriela Xavier <isgabdeveloper@gmail.com>
2024-10-31 16:11:01 -0300

    add package application

--- src/main/java/application/ErrorDuplicateTypedQuery.java
+++ src/main/java/application/ErrorDuplicateTypedQuery.java
@@ -1,33 +1,32 @@
-import entity.Guide;
+package application;
+
 import entity.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 
-public class TypedQueryLanguage {
+public class ErrorDuplicateTypedQuery {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hib");
 		EntityManager em = emf.createEntityManager();
-		
+
 		try {
-			TypedQuery<Guide> query = em.createNamedQuery("Guide.findById", Guide.class);
-			query.setParameter("id", 1);
-			Guide g = query.getSingleResult();
-			System.out.println(g);
-			
-			TypedQuery<Student> queryStu =  em.createNamedQuery("findById", Student.class);
+			/*
+			 * Named query tem escopo global, por isso não podemos ter elas com nomes repetidos
+			 */
+			TypedQuery<Student> queryStu = em.createNamedQuery("findById", Student.class);
 			queryStu.setParameter("id", 1);
 			Student s = queryStu.getSingleResult();
 			System.out.println(s);
-		
+
 		} catch (Exception e) {
 			if (em.getTransaction() != null) {
 				em.getTransaction().rollback();
 			}
 		} finally {
-			em.clear();
+			em.close();
 		}
 	}
 
--- src/main/java/application/TypedQueryLanguage.java
+++ src/main/java/application/TypedQueryLanguage.java
@@ -1,31 +1,34 @@
-
+package application;
+import entity.Guide;
 import entity.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 
-public class ErrorDuplicateTypedQuery {
+public class TypedQueryLanguage {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hib");
 		EntityManager em = emf.createEntityManager();
-
+		
 		try {
-			/*
-			 * Named query tem escopo global, por isso não podemos ter elas com nomes repetidos
-			 */
-			TypedQuery<Student> queryStu = em.createNamedQuery("findById", Student.class);
+			TypedQuery<Guide> query = em.createNamedQuery("Guide.findById", Guide.class);
+			query.setParameter("id", 1);
+			Guide g = query.getSingleResult();
+			System.out.println(g);
+			
+			TypedQuery<Student> queryStu =  em.createNamedQuery("findById", Student.class);
 			queryStu.setParameter("id", 1);
 			Student s = queryStu.getSingleResult();
 			System.out.println(s);
-
+		
 		} catch (Exception e) {
 			if (em.getTransaction() != null) {
 				em.getTransaction().rollback();
 			}
 		} finally {
-			em.close();
+			em.clear();
 		}
 	}
 
